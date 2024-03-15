00bdee1f3b13779cfd54ec5b35c4174e030752cb
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-15 21:38:36 +0000

    import views module into init package

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,8 @@ from flask import Blueprint, render_template
 app_views = Blueprint('app_view', __name__)
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
