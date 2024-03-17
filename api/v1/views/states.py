234198a66694a53ef7c33abd32ea0d3ad41c7749
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-17 21:26:53 +0000

    separate and break down get_states function

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,8 +11,9 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     '''Gets a state'''
-    states = State.all()
-    return jsonify([state.to_dict() for state in states])
+    states = storage.all(State).values()
+    list_of_states = [state.to_dict() for state in states]
+    return jsonify(list_of_states)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
