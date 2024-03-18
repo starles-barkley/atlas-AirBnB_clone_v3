dbcbfc58c4c5597eddc41fb67a11194bbefd8ee3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-18 00:18:10 +0000

    create delete amenity method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -23,3 +23,16 @@ def amenity_by_id(amenity_id):
     if not amenity:
         abort(404)
     return jsonify(amenity.to_dict())
+
+
+@app_views.route(
+    '/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    '''Deletes an amenity'''
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
