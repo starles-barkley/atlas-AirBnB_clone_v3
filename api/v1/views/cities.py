eff918f54a2907198d52f527c5fa2ed22e92795d
meddizzle316 <meddizzle316@gmail.com>
2024-03-18 12:38:54 -0500

    trying to appease the checker in cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,60 +4,47 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 
 
-@app_views.route("/states/<state_id>/cities", methods=['GET'])
-def get_cities(state_id=None):
+@app_views.route("/states/<state_id>/cities", methods=['GET', 'POST'])
+def get_cities(state_id):
     from models import storage
     from models.state import State
     from models.city import City
-    if state_id is None:
-        abort(404)
     state = storage.get(State, state_id)
     if state is None:
         abort (404)
-    all_cities = state.cities
-    if len(all_cities) < 1:
-        return []
-    return jsonify([city.to_dict() for city in all_cities])
+    if request.method == 'GET':
+        all_cities = state.cities
+        if len(all_cities) < 1:
+            return []
+        return jsonify([city.to_dict() for city in all_cities])
     
-@app_views.route("/states/<state_id>/cities", methods=['POST'], strict_slashes=True)
-def create_cities(state_id):
-    from models import storage
-    from models.state import State
-    from models.city import City
+    if request.method == 'POST':
 
-    # checking if state_id given is None
-    # if state_id is None:
-    #     abort(404, "state_id is none")
+        # uses get_json to parse http_body into dict
+        http = request.get_json(silent=True)
+        
+        # get_json returns None if it fails
+        if http is None:
+            abort(400, description='Not a JSON')
 
-    # checks if state_id is connected to state object
-    # state = storage.get(State, state_id)
-    # if state is None:
-    #     abort (404, "state object is none")
-
-    # uses get_json to parse http_body into dict
-    http = request.get_json(silent=True)
+        # checks if name is in http dict
+        if 'name' not in http:
+            abort(400, description='Missing name')
+        
+        city = City(**http)
+        # adds state_id to http dict
     
-    # get_json returns None if it fails
-    if http is None:
-        abort(404, 'Not a JSON')
+        setattr(city, "state_id", state_id)
+        
+        # init new City object with http dict
 
-    # checks if name is in http dict
-    if 'name' not in http.keys():
-        abort(400, 'Missing name')
-    
-    city = City(**http)
-    # adds state_id to http dict
-   
-    setattr(city, 'state_id', state_id)
-    
-    # init new City object with http dict
+        # performs new on object, updates and saves with
+        # Basemodel save method
+        city.save()
 
-    # performs new on object, updates and saves with
-    # Basemodel save method
-    city.save()
+        # returns jsonified dict of city object
+        return jsonify(city.to_dict()), 201
 
-    # returns jsonified dict of city object
-    return jsonify(city.to_dict()), 201
 
 
 @app_views.route("/cities/<city_id>", methods=['GET', 'PUT'])
