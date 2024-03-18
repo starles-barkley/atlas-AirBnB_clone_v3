bb98e15e2f3eff02b76af6752b86241fd3648ec9
meddizzle316 <meddizzle316@gmail.com>
2024-03-18 14:49:48 -0500

    reverting

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,12 +4,11 @@
 from flask import Blueprint, render_template
 
 
-app_views = Blueprint('app_view', __name__)
-if app_views is not None:    
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
+app_views = Blueprint('app_view', __name__)  
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
